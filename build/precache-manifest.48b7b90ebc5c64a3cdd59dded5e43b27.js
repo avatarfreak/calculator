dffb57c3d3a575678b9b6d9f1077fb4fb9871ad2
avatarfreak <avatarfreak>
2019-07-19 22:28:48 +0530

    package.json updated

---
+++ build/precache-manifest.48b7b90ebc5c64a3cdd59dded5e43b27.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "464d1f34f063324e5364f89780fcff3a",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "0e7333b0b703c56007e9",
+    "url": "/calculator/static/css/main.6179d0ce.chunk.css"
+  },
+  {
+    "revision": "83e8ac2490718537025d",
+    "url": "/calculator/static/js/2.e93c9a48.chunk.js"
+  },
+  {
+    "revision": "0e7333b0b703c56007e9",
+    "url": "/calculator/static/js/main.44ec883b.chunk.js"
+  },
+  {
+    "revision": "2fe8cbdbbea1fbf116e6",
+    "url": "/calculator/static/js/runtime~main.2539f434.js"
+  },
+  {
+    "revision": "ce6a1dcd365fd0caf5fe95ad93141fc2",
+    "url": "/calculator/static/media/n2d.ce6a1dcd.png"
+  }
+]);
